a6d821f9dc8780137c49dcf0c82da50fd14df8b1
Shubham Patidar <shubham.patidar@encoresky.com>
2022-09-22 19:53:03 +0530

    rename file name

--- convert-images-in-webp.php
+++ convert-images-in-webp.php
@@ -16,49 +16,8 @@
 
 /**
  * To prevent user to directly access your file.
- */ /**
-		* @var     object
-		* @access  private
-		* @since   1.0.0
-		*/
-		private static $_instance = null;
-
-		/**
-		* @var     string
-		* @access  public
-		* @since   1.0.0
-		*/
-		public $plugin_prefix = 'eur';
-
-		/**
-		 * The unique identifier of this plugin.
-		 *
-		 * @since    1.0.0
-		 * @access   protected
-		 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-		 */
-		protected $plugin_name;
-
-		/**
-		 * The current version of the plugin.
-		 *
-		 * @since    1.0.0
-		 * @access   protected
-		 * @var      string    $version    The current version of the plugin.
-		 */
-		protected $version;
-		
-		/**
-		 * Define the core functionality of the plugin.
-		 *
-		 * Set the plugin name and the plugin version that can be used throughout the plugin.
-		 * Load the dependencies, define the locale, and set the hooks for the admin area and
-		 * the public-facing side of the site.
-		 *$file
-		 * @since    1.0.0
-		 */
-		public function __c
-
+ 
+*/
 if( ! defined( 'ABSPATH' ) ) exit;
 
 /**
